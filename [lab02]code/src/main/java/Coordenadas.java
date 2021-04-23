636830f3ed24f5c27a33f04658cba4c0239b0931
eduardosalas1 <44951783+eduardosalas1@users.noreply.github.com>
2021-04-23 08:14:06 -0500

    Create Coordenadas.java

---
+++ [lab02]code/src/main/java/Coordenadas.java
@@ -0,0 +1,9 @@
+public class Coordenadas{
+    public int alto;
+    public int ancho;
+    
+    public Coordenadas(int alto,int ancho){
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+}
